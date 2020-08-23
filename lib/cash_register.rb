a54df013719eaab065624f1149332e78cb64089f
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-23 19:55:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
     @discount = discount
   end
   
-  def add_item(title,price,quantity)
+  def add_item(title,price,quantity = 1)
     @total = @total + price*quantity
   end
   
