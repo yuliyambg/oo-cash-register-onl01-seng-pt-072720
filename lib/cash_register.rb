8d3f75b9b4cf481dbb943c50c40be25207c72626
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-23 20:35:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,19 +1,31 @@
 
+
 class CashRegister
+  
   attr_accessor :total, :discount, :items
   
-  def initialize(discount = nil)
+  def initialize(discount = 0)
     @total = 0
     @discount = discount
     @items = []
   end
   
   def add_item(title,price,quantity = 1)
-    @items << title
-    @total = @total + price*quantity
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
+    else
+      @items << title
+    end
+    @total += price*quantity
+    @last_transaction_amount = price*quantity
+    @total
   end
-  
-def apply_discount()
+
+  def apply_discount()
     if @discount > 0
       @discount = @discount/100.to_f
       @total = @total - (@total * (@discount))
@@ -21,49 +33,12 @@ def apply_discount()
     else
       "There is no discount to apply."
     end
+
   end
 
+
+  def void_last_transaction()
+    @total -= @last_transaction_amount
+  end
+  
 end
-
-# class CashRegister
-  
-#   attr_accessor :total, :discount, :items
-  
-#   def initialize(discount = 0)
-#     @total = 0
-#     @discount = discount
-#     @items = []
-#   end
-  
-#   def add_item(title,price,quantity = 1)
-#     if quantity>1
-#       i=0
-#       while i<quantity
-#         @items << title
-#         i+=1
-#       end
-#     else
-#       @items << title
-#     end
-#     @total += price*quantity
-#     @last_transaction_amount = price*quantity
-#     @total
-#   end
-
-#   def apply_discount()
-#     if @discount > 0
-#       @discount = @discount/100.to_f
-#       @total = @total - (@total * (@discount))
-#       "After the discount, the total comes to $#{@total.to_i}."
-#     else
-#       "There is no discount to apply."
-#     end
-
-#   end
-
-
-#   def void_last_transaction()
-#     @total -= @last_transaction_amount
-#   end
-  
-# end
