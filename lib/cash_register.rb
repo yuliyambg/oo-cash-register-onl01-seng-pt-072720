08baf03b66ca906eb787bb46125bc7d523f97ec2
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-23 20:22:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,6 +17,7 @@ class CashRegister
       @total = @total - (@total * (@discount))
       "After the discount, the total comes to $#{@total.to_i}."
     else
+      "There is no discount to apply."
     end
   end
 
