61930b7e99fadb3282314f5f5be0a09f715871f5
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-23 20:24:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,8 +11,8 @@ class CashRegister
     @total = @total + price*quantity
   end
   
-  def apply_discount
-# if @discount > 0
+  def apply_discount()
+if @discount > 0
       @discount = @discount/100.to_f
       @total = @total - (@total * (@discount))
       "After the discount, the total comes to $#{@total.to_i}."
