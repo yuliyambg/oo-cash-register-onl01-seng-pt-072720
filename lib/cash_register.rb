4af2bcae2a295374ac0411cec47ed065ee0adf20
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-23 20:19:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,6 +15,7 @@ class CashRegister
  if @discount > 0
       @discount = @discount/100.to_f
       @total = @total - (@total * (@discount))
+      "After the discount, the total comes to $#{@total}."
     end
   end
 
